ee3577d4d1a5511f40f6c48762d9719554f34725
Rijan-Joshi <csaijspy@gmail.com>
2024-12-10 11:07:59 +0545

    Respond to multiple PING Commands

--- app/main.py
+++ app/main.py
@@ -6,8 +6,16 @@ def main():
 
     # Uncomment this to pass the first stage
     server_socket = socket.create_server(("localhost", 6379))
-    connection, address = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+    connection, _ = server_socket.accept() #Wait for client to connect
+
+    while True:
+        data = connection.recv(1024) #Receive the client input
+        
+        if not data: #Check if the data is b"" which is empty
+            break
+        
+        #Send the hardcode PONG response to the client
+        connection.sendall(b"+PONG\r\n")
 
     
 
