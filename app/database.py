d4a5139461c892cbd82b4f5627792ba0d8a51084
Rijan-Joshi <csaijspy@gmail.com>
2025-01-01 05:58:34 +0000

    Initial Replication ID and Offset

--- app/database.py
+++ app/database.py
@@ -12,7 +12,11 @@ class DataStore:
     def __init__(self, config: "RedisServerConfig"):
         self.config = config
         self._data: Dict[str, Tuple[str, Optional[int]]] = {}
-        self._replication_data = {"role": "master"}
+        self._replication_data = {
+            "role": "master",
+            "master_replid": "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb",
+            "master_repl_offset": 0,
+        }
 
     def set(self, key: str, value: str, expiry: Optional[int] = None) -> None:
         """Set a key-value pair with optional expiry (in milliseconds)."""
@@ -49,6 +53,7 @@ class DataStore:
         if self.config.replicaof is not None:
             self._replication_data["role"] = "slave"
 
-        line.append(f"role:{self._replication_data['role']}")
+        for key, value in self._replication_data.items():
+            line.append(f"{key}:{value}")
 
         return "\n".join(line)
