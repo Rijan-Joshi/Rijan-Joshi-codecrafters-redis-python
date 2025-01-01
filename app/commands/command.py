98e7f792049bc9dc0046978cd064c8ff895a1d51
Rijan-Joshi <csaijspy@gmail.com>
2025-01-01 18:43:12 +0000

    codecrafters submit [skip ci]

--- app/commands/command.py
+++ app/commands/command.py
@@ -7,8 +7,7 @@ from .connection import PINGCommand, ECHOCommand
 from app.protocol.resp_encoder import RESPEncoder
 from .strings import GETCommand, SETCommand, KEYSCommand, INFOCommand
 from .server import ConfigCommand
-
-# from .replication import REPLCONFCommand
+from .replication import REPLCONFCommand
 
 
 class CommandHandler:
@@ -28,7 +27,7 @@ class CommandHandler:
             "KEYS": KEYSCommand,
             "CONFIG": ConfigCommand,
             "INFO": INFOCommand,
-            # "REPLCONF": REPLCONFCommand,
+            "REPLCONF": REPLCONFCommand,
         }
 
     async def handle_command(self, args):
--- app/commands/replication.py
+++ app/commands/replication.py
@@ -1,12 +1,12 @@
-# from .base import Command
-# from app.utils.config import RedisServerConfig
+from .base import Command
+from app.utils.config import RedisServerConfig
 
 
-# class REPLCONFCommand(Command):
-#     def __init__(self, args, db, config: "RedisServerConfig"):
-#         super().__init__(args)
-#         self.config = config
+class REPLCONFCommand(Command):
+    def __init__(self, args, db, config: "RedisServerConfig"):
+        super().__init__(args)
+        self.config = config
 
-#     async def execute(self):
-#         print("Okay, I am here.")
-#         return self.encoder.encode_simple_string("OK")
+    async def execute(self):
+        print("Okay, I am here.")
+        return self.encoder.encode_simple_string("OK")
