35d62ca54cd361d05b0e0f76ca07dee2557ff6ea
Rijan-Joshi <csaijspy@gmail.com>
2025-01-17 10:41:17 +0000

    Multiple transactions #jf8

--- app/commands/command_state.py
+++ app/commands/command_state.py
@@ -5,4 +5,10 @@ from dataclasses import dataclass
 @dataclass
 class CommandState:
     should_be_queued: bool = False
-    command_queue: asyncio.Queue = asyncio.Queue()
+    command_queue: asyncio.Queue = None
+
+    def __post_init__(self):
+        # Create a new queue for each instance
+
+        if self.command_queue is None:
+            self.command_queue = asyncio.Queue()
